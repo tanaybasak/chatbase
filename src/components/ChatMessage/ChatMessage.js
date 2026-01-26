6c39d8253a4897350d507636a4ef006b64282dfd
Basak, Tanoy <tbasak+levi@levi.com>
2026-01-26 18:23:49 +0530

    markdown

--- src/components/ChatMessage/ChatMessage.js
+++ src/components/ChatMessage/ChatMessage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import './ChatMessage.scss';
 
 const ChatMessage = ({ message, isUser }) => {
@@ -8,7 +10,37 @@ const ChatMessage = ({ message, isUser }) => {
         {isUser ? '👤' : '🤖'}
       </div>
       <div className="message-content">
-        <div className="message-text">{message}</div>
+        <div className="message-text">
+          {isUser ? (
+            message
+          ) : (
+            <ReactMarkdown 
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code: ({node, inline, className, children, ...props}) => {
+                  return inline ? (
+                    <code className="inline-code" {...props}>
+                      {children}
+                    </code>
+                  ) : (
+                    <pre className="code-block">
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    </pre>
+                  );
+                },
+                table: ({node, ...props}) => (
+                  <div className="table-wrapper">
+                    <table {...props} />
+                  </div>
+                ),
+              }}
+            >
+              {message}
+            </ReactMarkdown>
+          )}
+        </div>
       </div>
     </div>
   );
